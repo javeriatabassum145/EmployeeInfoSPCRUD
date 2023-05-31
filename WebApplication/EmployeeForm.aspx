bd49112b2d28cd68e867a12888fce7fdad7048b1
Javeria Tabassum <javeria.tabassum@tpsonline.com>
2023-05-31 12:20:35 +0500

    project added

---
+++ WebApplication/EmployeeForm.aspx
@@ -0,0 +1,116 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EmployeeForm.aspx.cs" Inherits="WebApplication.EmployeeForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 226px;
+        }
+        .auto-style2 {
+            width: 182px;
+        }
+        .auto-style5 {
+            width: 784px;
+        }
+        .auto-style6 {
+            width: 5px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h3>Employee Form</h3></div>
+        <table style="width: 99%; height: 189px;">
+            <tr>
+                <td class="auto-style1">Employee Name</td>
+                <td class="auto-style5">
+                    <asp:TextBox ID="empName" runat="server" Height="25px" Width="338px"></asp:TextBox>
+                </td>
+                <td class="auto-style6">&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Employee Age</td>
+                <td class="auto-style5">
+                    <asp:TextBox ID="empAge" runat="server" Height="25px" Width="338px"></asp:TextBox>
+                </td>
+                <td class="auto-style6">&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Employee Gender</td>
+                <td class="auto-style5">
+                    <asp:TextBox ID="empGender" runat="server" Height="25px" Width="338px"></asp:TextBox>
+                </td>
+                <td class="auto-style6">&nbsp;</td>
+            </tr>
+        </table>
+            <table style="width: 100%; height: 50px;">
+                <tr>
+                    <td class="auto-style2">
+                        <asp:Button ID="Button1" OnClick="CreateNewEmployee" runat="server" Height="27px" Text="Add Employee" Width="137px" BackColor="White" ForeColor="Black" />
+                    </td>
+                </tr>
+            </table>
+        <asp:Label ID="Label1" runat="server"></asp:Label> 
+        <h2>Employee Data</h2>
+ 
+
+        <p>
+            <asp:GridView ID="GridView1" runat="server" DataKeyNames="ID" AutoGenerateColumns="False" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" OnRowDeleting="GridView1_RowDeleting" Width="853px">
+                 <Columns>  
+                <asp:TemplateField>  
+                    <ItemTemplate>  
+                        <asp:Button ID="btn_Edit" runat="server" Text="Edit" CommandName="Edit" />  
+                        <asp:Button ID="btn_Delete" runat="server" Text="Delete" CommandName="Delete" />  
+
+                    </ItemTemplate>  
+                    <EditItemTemplate>  
+                        <asp:Button ID="btn_Update" runat="server" Text="Update" CommandName="Update"/>  
+                        <asp:Button ID="btn_Cancel" runat="server" Text="Cancel" CommandName="Cancel"/>  
+                    </EditItemTemplate>  
+                </asp:TemplateField>  
+                <asp:TemplateField HeaderText="ID">  
+                    <ItemTemplate>  
+                        <asp:Label ID="lbl_ID" runat="server" Text='<%#Eval("ID") %>'></asp:Label>  
+                    </ItemTemplate>  
+                </asp:TemplateField>  
+                <asp:TemplateField HeaderText="Name">  
+                    <ItemTemplate>  
+                        <asp:Label ID="lbl_Name" runat="server" Text='<%#Eval("Name") %>'></asp:Label>  
+                    </ItemTemplate>  
+                    <EditItemTemplate>  
+                        <asp:TextBox ID="txt_Name" runat="server" Text='<%#Eval("Name") %>'></asp:TextBox>  
+                    </EditItemTemplate>  
+                </asp:TemplateField>  
+                <asp:TemplateField HeaderText="Age">  
+                    <ItemTemplate>  
+                        <asp:Label ID="lbl_Age" runat="server" Text='<%#Eval("Age") %>'></asp:Label>  
+                    </ItemTemplate>  
+                    <EditItemTemplate>  
+                        <asp:TextBox ID="txt_Age" runat="server" Text='<%#Eval("Age") %>'></asp:TextBox>  
+                    </EditItemTemplate>  
+                </asp:TemplateField>  
+                                     <asp:TemplateField HeaderText="Gender">  
+                    <ItemTemplate>  
+                        <asp:Label ID="lbl_Gender" runat="server" Text='<%#Eval("Gender") %>'></asp:Label>  
+                    </ItemTemplate>  
+                    <EditItemTemplate>  
+                        <asp:TextBox ID="txt_Gender" runat="server" Text='<%#Eval("Gender") %>'></asp:TextBox>  
+                    </EditItemTemplate>  
+                </asp:TemplateField>  
+            </Columns>  
+            <HeaderStyle BackColor="#663300" ForeColor="#ffffff"/>  
+            <RowStyle BackColor="#e7ceb6"/> 
+            </asp:GridView>
+        </p>
+ 
+
+    </form>
+
+    
+</body>
+</html>
---
+++ WebApplication/EmployeeForm.aspx.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data; 
+
+namespace WebApplication
+{
+    public partial class EmployeeForm : System.Web.UI.Page
+    {
+        //Connection String
+        string ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GridViewBind();
+            }
+        }
+
+        //Create Operation
+        protected void CreateNewEmployee(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("InsertEmployeeSP", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@Name", empName.Text);
+                    cmd.Parameters.AddWithValue("@Age", empAge.Text);
+                    cmd.Parameters.AddWithValue("@Gender", empGender.Text);
+
+                    connection.Open();
+                    int status = cmd.ExecuteNonQuery();
+
+                    if (status != 0)
+                    {
+                        Label1.Text = "Record Inserted Succesfully into the Database";
+                        Label1.ForeColor = System.Drawing.Color.CornflowerBlue;
+                    }
+                }
+                GridViewBind();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong." + ex);
+            }
+
+        }
+
+        /*
+         //Read Operation
+        protected void ReadEmployees(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+
+            try
+            {
+                string query = "select * from [AdoNetDb].[dbo].[Employee]";
+                SqlCommand sc = new SqlCommand(query, connection);
+                // Opening connection  
+                connection.Open();
+                SqlDataReader reader = sc.ExecuteReader();
+                GridView1.DataSource = reader;
+                GridView1.DataBind(); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong." + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }*/
+
+        protected void GridViewBind()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("GetEmployeeSP", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                connection.Close();
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    GridView1.DataSource = ds;
+                    GridView1.DataBind();
+                }
+
+                /*  else
+                  {
+                      ds.Tables[0].Rows.Add(ds.Tables[0].NewRow());
+                      GridView1.DataSource = ds;
+                      GridView1.DataBind();
+                      int columncount = GridView1.Rows[0].Cells.Count;
+                      GridView1.Rows[0].Cells.Clear();
+                      GridView1.Rows[0].Cells.Add(new TableCell());
+                      GridView1.Rows[0].Cells[0].ColumnSpan = columncount;
+                      GridView1.Rows[0].Cells[0].Text = "No Records Found";
+                  }*/
+            }
+        }  
+
+        // Update Operation
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            GridViewBind();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                Label id = GridView1.Rows[e.RowIndex].FindControl("lbl_ID") as Label;
+
+                int userid = Convert.ToInt32(id.Text);
+                TextBox Name = GridView1.Rows[e.RowIndex].FindControl("txt_Name") as TextBox;
+                TextBox Age = GridView1.Rows[e.RowIndex].FindControl("txt_Age") as TextBox;
+                TextBox Gender = GridView1.Rows[e.RowIndex].FindControl("txt_Gender") as TextBox;
+
+                String NameEmp = Name.Text;
+                int AgeEmp = Convert.ToInt32(Age.Text);
+                String GenderEmp = Gender.Text;
+
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("UpdateEmployeeSP", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@ID", userid);
+                    cmd.Parameters.AddWithValue("@Name", NameEmp);
+                    cmd.Parameters.AddWithValue("@Age", AgeEmp);
+                    cmd.Parameters.AddWithValue("@Gender", GenderEmp);
+
+                    connection.Open();
+                    int status = cmd.ExecuteNonQuery();
+
+                    if (status != 0)
+                    {
+                        Label1.Text = "Record Updated Succesfully";
+                        Label1.ForeColor = System.Drawing.Color.Green;
+                    }
+                }
+                GridView1.EditIndex = -1;
+                GridViewBind();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong." + ex);
+            }
+        }
+        
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            GridViewBind();
+        }  
+
+        //Delete Operation
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                int userid = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+                GridViewRow row = (GridViewRow)GridView1.Rows[e.RowIndex];
+                
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("DeleteEmployeeSP", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@ID", userid);
+                    connection.Open();
+                    int status = cmd.ExecuteNonQuery();
+                    if (status != 0)
+                    {
+                        Label1.Text = "Record Deleted Succesfully";
+                        Label1.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+                GridViewBind();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong." + ex);
+            }
+        }
+    }
+}
